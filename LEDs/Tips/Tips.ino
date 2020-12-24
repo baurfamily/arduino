44a746da65b929067fb56d4cf8c226642eea18a1
Allison Baur <>
2020-12-23 16:49:33 -0800

    got tip color going down and back

--- LEDs/Tips/Tips.ino
+++ LEDs/Tips/Tips.ino
@@ -5,26 +5,50 @@ CRGB leds[LED_COUNT];
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, 6>(leds,  LED_COUNT);
+  //Serial.begin(115200);
 }
 
 void loop() {
   static int len = 0;
-  len = (len + 1)%11;
-  leds[7] = CRGB(255, 0, 0);
-  leds[8] = CRGB(255, 0, 0);
-  leds[9] = CRGB(255, 0, 0);
-  leds[10] = CRGB(255, 0, 0);
-  leds[11] = CRGB(255, 0, 0);
-  leds[12] = CRGB(255, 0, 0);
+  static int hue = 0;
+  static bool forward = true;
   
+  if (forward) {
+    len++;
+  } else {
+    len--;
+  }
+  //Serial.println(len);
+
+  if (len == 10){
+    forward = false;
+    hue = (hue + 32)%256;
+  } else if (len == 0) {
+    forward = true;
+    hue = (hue + 32)%256;
+  }
   for(int i = 0; i<5; i++) {
-    int pos = 20*i+10-len;
-    for(int j = 0; j<len*2; j++){
-      leds[pos + j] = CRGB(255, 0, 0);
+    int center = 20*i+10;
+    int pos = center-len;
+    
+    if (forward) {
+      for(int j = 0; j<len*2+1; j++){
+        leds[pos + j] = CHSV(hue, 255, 100);
+      }
+    } else {
+      int start = center-10;
+      for (int j = start; j<pos; j++) {
+        leds[j] = CHSV(hue, 255, 100);
+      }
+      int endCap = center + 10;
+      int endPos = center+len;
+      for (int j = endPos; j<endCap; j++) {
+        leds[j] = CHSV(hue, 255, 100);
+      }
     }
   }
 
 
     FastLED.show();
-      delay(100);
+    delay(100);
 }
