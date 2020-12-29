88d9dac95c606c3b080481f7da8d6b1713d7ea32
Eric Baur <baurfamily@gmail.com>
2020-12-28 16:29:51 -0800

    fade blink working!

--- Experiments/ArrayBlink/ArrayBlink.ino
+++ Experiments/ArrayBlink/ArrayBlink.ino
@@ -15,21 +15,21 @@ void pulseOnce(int muSec) {
 }
 
 void pulseStrip(int pattern[]) {
-  Serial.print("pulseStrip: ");
+//  Serial.print("pulseStrip: ");
   
   digitalWrite(LED_BUILTIN, HIGH);
   for (int i=0; i<PACKET_SIZE; i++) {
-    if (i%8 == 0) { Serial.print("_"); }
-    Serial.print(pattern[i]);
+//    if (i%8 == 0) { Serial.print("_"); }
+//    Serial.print(pattern[i]);
 
-    delayMicroseconds(8);
+    delayMicroseconds(4);
     if (pattern[i] == 1) {
       digitalWrite(LED_BUILTIN, LOW);
     }
     digitalWrite(LED_BUILTIN, HIGH);
   }
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("");
+//  Serial.println("");
 }
 
 void loop() {
@@ -39,14 +39,15 @@ void loop() {
   int pattern[PACKET_SIZE] = { 1, 1, 1, 1, 1, 1, 1, 1,
                                1, 0, 0, 0, 0, 0, 0, 0 };
 
-//  for(int i; i<50; i++) {
-//    for(int j=0; j<PACKET_SIZE; j++) {
-//      pattern[j] = random(2);
-//    }
-//    delayMicroseconds(10);
-//    pulseStrip(pattern );
-//  }
+  for(int i; i<50; i++) {
+    for(int j=0; j<PACKET_SIZE; j++) {
+      pattern[j] = random(2);
+    }
+    pulseStrip(pattern);
+    delayMicroseconds(10);
+//    delay(1000);
+  }
 
-  pulseStrip(pattern);
-  delay(1000);
+//  pulseStrip(pattern);
+//  delay(1000);
 }
---
+++ Experiments/FadeBlinky/FadeBlinky.ino
@@ -0,0 +1,71 @@
+#define PACKET_SIZE 8
+#define PACKET_DELAY 20
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(115200);
+  pulseStrip(1);
+}
+
+void pulseOnce(int muSec) {
+  delayMicroseconds(muSec);
+  digitalWrite(LED_BUILTIN, LOW);
+  delayMicroseconds(muSec);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void pulseStrip(int count) {
+  digitalWrite(LED_BUILTIN, HIGH);
+//  Serial.print("pulse:");
+  for(int i=0; i<count; i++) {
+//    Serial.print(".");
+    delayMicroseconds(PACKET_DELAY);
+    digitalWrite(LED_BUILTIN, LOW);
+    delayMicroseconds(PACKET_DELAY);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+//  Serial.println("");
+}
+
+void pulseStrip(int pattern[]) {
+//  Serial.print("pulseStrip: ");
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  for (int i=0; i<PACKET_SIZE; i++) {
+//    if (i%8 == 0) { Serial.print("_"); }
+//    Serial.print(pattern[i]);
+
+    delayMicroseconds(PACKET_DELAY);
+    if (pattern[i] == 1) {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+//  Serial.println("");
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  static int brightness = 0;
+  static bool up = true;
+
+  int color = 6;
+
+//  static int redFromReset[PACKET_SIZE] = { 1, 1, 0, 0, 0, 0, 0, 0 };
+//  static int resetFromRed[PACKET_SIZE] = { 1, 1, 1, 1, 1, 1, 0, 0 };
+
+  if (up) { brightness++; } else { brightness--; }
+  if (brightness == 0 || brightness == 255) { up = !up; }
+
+  int val = map( brightness, 0, 255, 1000, 15000);
+  
+  pulseStrip(color);
+  delayMicroseconds(val);
+
+  pulseStrip(8-color);
+  delayMicroseconds(15000 - val);
+
+}
