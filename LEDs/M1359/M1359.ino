01a6b66df09e96c77dde55fe0b0f7d1ea07a285e
Eric Baur <baurfamily@gmail.com>
2020-12-31 11:50:38 -0800

    created some static arrays

--- LEDs/M1359/M1359.ino
+++ LEDs/M1359/M1359.ino
@@ -1,20 +1,92 @@
-
-#define VCC_PIN 13
+#define VCC_PIN 11
 
 #include <M1359.h>
 
 M1359Strip strip(VCC_PIN);
 
 void setup() {
+  pinMode(12, INPUT);
+
   strip.reset();
   strip.on();
-  strip.setColor(M1359_BLUE);
   Serial.begin(115200);
   Serial.println("Setup complete");
+ 
+  strip.setColor(M1359_PURPLE);
+  strip.setBrightness(255);
 }
 
 void loop() {
-  loopCycle();
+  loopRandomArray();
+}
+
+#define SIZE 128
+void loopRandomArray() {
+  static byte pattern[SIZE] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+  };
+
+  //red-green-red
+  // static byte pattern[SIZE] = {
+  //   0,0,1,0,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,1,1,0,1,1,1,1,0,0,1,1,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,0,1,0,1
+  // };
+
+  //multi-color?
+  // static byte pattern[SIZE] = {
+  //   1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,0,0,0,1,1,0,0,1,1,0,1,1,1,1,1,1,1,0,1,1,0,1,1,1,0,1,1,1,1,0,0,0,0,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,0,0,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,0,0,1,1
+  // };
+
+  //three color
+  // static byte pattern[SIZE] = {
+  //   1,1,1,1,1,0,1,1,0,1,1,0,0,1,1,1,1,0,1,0,0,1,0,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,0,0,0,1,1,1,1,0,0,0,1,1,0,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,0,0,0,0,1,0,1,1,1,1,0,1,0,1,1,1,0,0,1,1,0,0,0,0,0,1,1,1,0,1,0,1,0,0,1,1,0,1
+  // };
+
+  // for(int i=0; i<SIZE; i++) {
+  //   pattern[i] = (i%4 == 1 ? 1 : 0);
+  // }
+  for(int i=4; i<SIZE; i++) {
+    if (random(10) > 2) {
+      pattern[i] = 1;
+    } else {
+      pattern[i] = 0;
+    }
+    // pattern[i] = random(2);
+  }
+
+  Serial.print("pattern: ");
+  for(int i=0; i<SIZE; i++) {
+    Serial.print(pattern[i]);
+  }
+  Serial.println("");
+
+  // for(int i=500; i<750; i += 10) {
+    strip.quickPulse(pattern, SIZE, 750);
+  // }
+
+  delay(5000);
+}
+
+void loopChaos() {  
+  // strip.display();
+
+  analogWrite(11, random(64, 200));
+
+  delayMicroseconds(750);
+
+  digitalWrite(11, HIGH);
+
+  delay(1000);
+
+  // digitalWrite(13, LOW);
+  // delay(20);
+  // digitalWrite(13, HIGH);
+
+  // pinMode(12, OUTPUT);
+  // digitalWrite(12, LOW);
+  // delay(500);
+  // pinMode(12, INPUT);
+
+  delay(1000);
 }
 
 void loopColorFade() {
@@ -47,15 +119,16 @@ void loopCycle() {
 void loopFade() {
   static int brightness = 0;
   static int color = 1;
-  static int increment = 1;
+  static int increment = 4;
 
   brightness += increment;
   
-  if (brightness == 0 || brightness == 255) {
+  if (brightness <= 0 || brightness >= 255) {
     increment = -increment;
+    brightness += increment;
   }
 
-  if (brightness == 0) {
+  if (brightness <= increment) {
     color = (color+1)%8;
     strip.setColor( color );
     Serial.print("Setting color: ");
